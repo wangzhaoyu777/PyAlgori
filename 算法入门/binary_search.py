053f505f4aab7d37000cab3f870d8c61232fd1af
Joey <wangzhaoyu777@gmail.com>
2023-05-03 15:24:14 -0400

    add binary search practice

---
+++ 算法入门/binary_search.py
@@ -0,0 +1,19 @@
+# Zhaoyu Wang developed
+# time: 2023-05-03 2:57 p.m.
+a = [1, 2, 3, 4, 5, 6]
+b = [1, 2, 3, 4, 5, 6, 7]
+
+def binary_search(data_list, value):
+    left = 0
+    right = len(data_list) - 1
+    while left <= right:
+        index = (left + right) // 2
+        if data_list[index] == value:
+            return print(index)
+        elif data_list[index] > value:
+            right = index -1
+        else:# data_list[index] < value:
+            left = index +1
+    else:
+        return print('None')
+binary_search(b, 4)
---
+++ 算法入门/linear_search.py
@@ -0,0 +1,13 @@
+# Zhaoyu Wang developed
+# time: 2023-05-03 1:13 p.m.
+# def linear_search(li, value):
+#     for index, v in enumerate(li):
+#         if v == value:
+#             return print(index)
+#     else:
+#         return print('None')
+def linear_search(data_list, value):
+    for i in range(len(data_list)):
+        if value == data_list[i]:
+            return print(i)
+linear_search([1,2,3,4,5,6], 2)

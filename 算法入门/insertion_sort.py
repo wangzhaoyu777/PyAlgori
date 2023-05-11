2985f7498a8f2f02b03051f2a9ee7d8fbe5607a2
wangzhaoyu777 <42188901+wangzhaoyu777@users.noreply.github.com>
2023-05-10 23:19:47 -0400

    Update insertion_sort.py
    comment added

--- 算法入门/insertion_sort.py
+++ 算法入门/insertion_sort.py
@@ -3,9 +3,9 @@
 
 # ?????????
 def insert_sort(li):
-    for i in range(1, len(li)):
-        temp = li[li]
-        j = i -1
+    for i in range(1, len(li)):         # 遍历除了第一位以外的元素和第一位比大小，或者说是和i-1比大小；i指摸到的牌的下标
+        temp = li[i]                    # 
+        j = i -1                        # j指手里牌的下标
         while j >= 0 and li[j] > temp:
             li[j+1] = li[j]
             j-=1
